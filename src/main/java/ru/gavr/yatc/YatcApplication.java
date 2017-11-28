393821fb9f3bbea4e9bc6d6712347141fa706bab
Alexander Gavrilenko <agavrilenko2.0@gmail.com>
2017-11-28 18:14:42 +0300

    Init commit

---
+++ src/main/java/ru/gavr/yatc/YatcApplication.java
@@ -0,0 +1,12 @@
+package ru.gavr.yatc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YatcApplication.class, args);
+	}
+}
