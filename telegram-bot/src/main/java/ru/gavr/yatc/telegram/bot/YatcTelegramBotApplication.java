98114e66a9d9dea2a3f2647151d31e7ec0afc5b5
Alexander Gavrilenko <agavrilenko2.0@gmail.com>
2018-02-15 11:03:53 +0300

    Add multi modules configuration

---
+++ telegram-bot/src/main/java/ru/gavr/yatc/telegram/bot/YatcTelegramBotApplication.java
@@ -0,0 +1,11 @@
+package ru.gavr.yatc.telegram.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YatcTelegramBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YatcTelegramBotApplication.class, args);
+    }
+}
